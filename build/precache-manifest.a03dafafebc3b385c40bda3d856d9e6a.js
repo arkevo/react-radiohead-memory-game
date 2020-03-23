c458f96dcafd9cc21f9324b98027a10ba9822eac
Ivo Kolev <arkevo@Ivos-MacBook-Pro.local>
2020-03-22 22:24:55 -0400

    Pushed final version

---
+++ build/precache-manifest.a03dafafebc3b385c40bda3d856d9e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977ab4f3848a8b7a7e7bb862af52497f",
+    "url": "/react-radiohead-memory-game/index.html"
+  },
+  {
+    "revision": "09193b7e23d8b9d1a48a",
+    "url": "/react-radiohead-memory-game/static/css/main.1515abf8.chunk.css"
+  },
+  {
+    "revision": "ddc13064326841b6ffe2",
+    "url": "/react-radiohead-memory-game/static/js/2.2f723c20.chunk.js"
+  },
+  {
+    "revision": "78b86c0095c74f94992db9795d80e258",
+    "url": "/react-radiohead-memory-game/static/js/2.2f723c20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09193b7e23d8b9d1a48a",
+    "url": "/react-radiohead-memory-game/static/js/main.cd850ed9.chunk.js"
+  },
+  {
+    "revision": "120ac5e84e3623a28eb4",
+    "url": "/react-radiohead-memory-game/static/js/runtime-main.06f58ee8.js"
+  }
+]);
